2f539b49bf46f99fef1ead63b31de496cc62b688
ExpressGradient <johndoeknown@protonmail.com>
2021-07-01 14:15:39 +0530

    modified interfaces and added allowed methods

--- index.ts
+++ index.ts
@@ -1,22 +1,56 @@
-import { error, info } from "https://deno.land/std@0.100.0/log/mod.ts";
-
 import { RunServerPropsType } from "./types.ts";
 import { routeExtract } from "./utils/route_extract.ts";
 
 export const run = async (props: RunServerPropsType) => {
   for await (const request of props.server) {
-    const extract = routeExtract({ url: request.url });
-    if (
-      "table" in extract && extract.table !== "" && "operation" in extract &&
-      extract.operation
-    ) {
-      info(extract);
-      request.respond({ body: "Valid Query" });
+    // Check for Request Method
+    if (request.method === "POST") {
+      // Extract Table and Operation
+      const extract = routeExtract({ url: request.url });
+
+      // Check if the URL contains the Table and an Operation
+      if (
+        extract.table && extract.table !== "" && extract.operation
+      ) {
+        // Find the table from config.
+        const table = props.tables[extract.table];
+
+        // Check if the Table exists in the Config
+        if (table) {
+          // Check if the Operation is allowed on the Table
+          if (
+            table.operations[extract.operation] &&
+            table.operations[extract.operation].allowed
+          ) {
+            request.respond({ body: "Thank You" });
+          } else {
+            // Respond with Method Not Allowed Error when the Operation is not allowed on the Table
+            request.respond({
+              status: 405,
+              body:
+                `${extract.operation} Operation is not allowed on ${extract.table} Table`,
+            });
+          }
+        } else {
+          // Respond with Not Found Error when the Table is not specified in the Config
+          request.respond({
+            status: 404,
+            body: `${extract.table} Table not specifed in the Config`,
+          });
+        }
+      } else {
+        // Respond with Bad Request Error when Table and Operation are missing or invalid
+        request.respond({
+          status: 400,
+          body: "Invalid Query: Table/Operation is missing or invalid",
+        });
+      }
     } else {
-      error({ error: "Table/Operation is missing or invalid" });
+      // Respond with Method Not Allowed Error when a non POST Request is made
       request.respond({
-        status: 400,
-        body: "Invalid Query: Table/Operation is missing or invalid",
+        status: 405,
+        body:
+          `EzAPI accepts only POST Request. ${request.method} Request was made.`,
       });
     }
   }
--- types.ts
+++ types.ts
@@ -2,6 +2,32 @@ import { Server } from "https://deno.land/std@0.100.0/http/server.ts";
 
 export interface RunServerPropsType {
   server: Server;
+  connections: {
+    postgres: string;
+    redis?: string;
+  };
+  logging?: boolean;
+  tables: Record<string, Table>;
+}
+
+export interface Table {
+  operations: Operations;
+}
+
+interface OperationsKeys {
+  [key: string]: Operation;
+}
+
+export interface Operations extends OperationsKeys {
+  create: Operation;
+  read: Operation;
+  update: Operation;
+  delete: Operation;
+}
+
+export interface Operation {
+  allowed?: boolean;
+  onRequest?: () => void;
 }
 
 export interface RouteExtractPropsType {
